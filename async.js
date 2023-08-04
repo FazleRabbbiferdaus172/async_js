2191ab440e22398de09e69ffb7ed8bf12c4d95e8
Fazle Rabbi Ferdas <fazle.ferdaus1416@gmail.com>
2023-08-05 05:09:55 +0600

    working state of reverse coding the geo location and finind the country details

--- async.js
+++ async.js
@@ -1,1 +1,16 @@
-console.log('Hello world')
+function whereAmI(lat, lng){
+    let x;
+    fetch(`https://geocode.xyz/${lat},${lng}?json=1&auth=809769886241866428498x37494`)
+    .then((res) => res.json())
+    .then((dataGeo) => {
+        console.log(dataGeo);
+        return fetch(`https://restcountries.eu/rest/v2/name/${dataGeo.country}`);
+    })
+    .then((res) => res.json())
+    .then((data) => {
+        console.log(data);
+        return data;
+    })
+    .catch((err) => err);
+}
+whereAmI(51.50354,-0.12768);
