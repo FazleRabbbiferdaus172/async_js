e0181ea5f264cf75e466f284d20a42f97c56d2a2
Fazle Rabbi Ferdas <fazle.ferdaus1416@gmail.com>
2023-08-05 18:17:56 +0600

    challeneg 2 of async js is complete.

--- async.js
+++ async.js
@@ -1,1 +1,47 @@
-console.log('Hello world')
+const imageContainer = document.querySelector("#app");
+
+function wait(seconds) {
+  return new Promise(function (resolve, reject) {
+    setTimeout(resolve, seconds * 1000);
+  });
+}
+
+function createImage(img_path) {
+  return new Promise(function (resolve, reject) {
+    const img = document.createElement("img");
+    img.src = img_path;
+    img.addEventListener("load", function () {
+      resolve(img);
+    });
+
+    img.addEventListener("error", function () {
+      reject(new Error("Image not found"));
+    });
+  });
+}
+
+function ImagePhaseInOut(img) {
+  let currentImage;
+  return createImage(img)
+    .then((img) => {
+      currentImage = img;
+      imageContainer.append(img);
+      return wait(2);
+    })
+    .then(() => {
+      currentImage.style.display = "none";
+    })
+    .catch((err) => console.error(err));
+}
+
+const images_array = [
+  "images/1.jpg",
+  "images/2.jpg",
+  "images/3.jpg",
+  "images/4.jpg",
+];
+
+
+for (let i=0, p = Promise.resolve(); i < 4; i++) {
+    p = p.then(() => ImagePhaseInOut(images_array[i]));
+}
